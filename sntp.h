78af00b93dd6113a4cee4c1bfadcb850e6df1f72
Yuanze-li <yuanze.li@alumnos.upm.es>
2026-03-10 12:25:28 +0100

    1

---
+++ sntp.h
@@ -0,0 +1,12 @@
+#ifndef __SNTP_H
+#define __SNTP_H
+#include "cmsis_os2.h"
+#include "RTE_Components.h"
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+
+
+
+int Init_SNTP(void);
+#endif
